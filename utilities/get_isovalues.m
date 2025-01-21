d92e2c6777cee1a3badadedeac9ead4657a777ed
Ben Magruder <magru018@umn.edu>
2025-01-21 15:26:10 -0600

    Clean up get_isovalues

--- utilities/get_isovalues.m
+++ utilities/get_isovalues.m
@@ -30,7 +30,7 @@ function isovalue = get_isovalues(R,dim,options)
     gap_close = 1; %Toggle the white space closing feature (Default = 1)
     plot_grid = zeros(1,dim);
     for ig = 1:dim
-        plot_grid(ig) = grid(ig)+1;
+        plot_grid(ig) = grid(ig) + 1;
     end
 
     isovalue = zeros(1,n_mnr);
@@ -47,14 +47,13 @@ function isovalue = get_isovalues(R,dim,options)
         l_length(in) = polmaxa(in) -polmin(in);
     end
 
-
     %Creating the scaled matrix, S
     S = R;
     for in = 1:n_mnr
         for iz=1:grid(3)+1
             for iy=1:grid(2)+1
                 for ix=1:grid(1)+1
-                    S (ix,iy,iz,in) = weight(in)*(R(ix,iy,iz,in)-polmin(in))/l_length(in); % Matrix with scaled values
+                    S(ix,iy,iz,in) = weight(in)*(R(ix,iy,iz,in)-polmin(in))/l_length(in); % Matrix with scaled values
                 end
             end
         end
@@ -62,7 +61,7 @@ function isovalue = get_isovalues(R,dim,options)
 
     % Finding the location of the polmax points
     pmax_loc = zeros(n_lines,3,n_mnr);
-    for in =1:n_mnr
+    for in = 1:n_mnr
         [si,sj,sk] = ind2sub([plot_grid n_mnr],find(S(:,:,:,in) == weight(in),n_lines));
         pmax_loc(:,:,in) = [si,sj,sk];
     end
@@ -96,15 +95,15 @@ function isovalue = get_isovalues(R,dim,options)
             iy(ir,il) = start_coord(2)+ round((il-1)*(dir_vec(ir,2)/step_length(ir)));
             iz(ir,il) = start_coord(3)+ round((il-1)*(dir_vec(ir,3)/step_length(ir)));
             counter = counter + 1;
-            for in= 1:n_mnr
-                line_vals(in,counter) = R(ix(ir,il),iy(ir,il),iz(ir,il),in);
+            for in = 1:n_mnr
+                line_vals(in,counter) = R(ix(ir,il), iy(ir,il), iz(ir,il), in);
             end
         end
     end
     x_plot = linspace(1,counter+1,counter+1);
 
     % The final point
-    for in= 1:n_mnr
+    for in = 1:n_mnr
         line_vals(in,counter+1)= R(end_coord(1),end_coord(2),end_coord(3),in);
     end
 
@@ -129,12 +128,12 @@ function isovalue = get_isovalues(R,dim,options)
     k = 0;
     involved = zeros(n_mnr,n_mnr-1);
 
-    while n_mnr-loop_round > 0
+    while (n_mnr - loop_round > 0)
 
         for j = 1:n_mnr-loop_round
             k = k+1;
-            involved(loop_round,j)= k;  %Needed for isovalue calculation
-            involved(loop_round+j,n_mnr-j)= k;
+            involved(loop_round,j) = k;  %Needed for isovalue calculation
+            involved(loop_round+j,n_mnr-j) = k;
             % Check if the lines intersect:
             if ~isempty(find(diff(sign(line_new(loop_round+j,:)-line_new(loop_round,:))), 1))
                 x_inter_store{k} = find(diff(sign(line_new(loop_round+j,:)-line_new(loop_round,:))));
@@ -143,10 +142,10 @@ function isovalue = get_isovalues(R,dim,options)
                 % interpolate to find the y value where it intersects
                 for i = 1:length(cell2mat(x_inter_store(k)))
                     x_inter = cell2mat(x_inter_store(k));
-                    p(1,1)= line_new(loop_round,x_inter(i));
-                    p(1,2)= line_new(loop_round,1+x_inter(i));
-                    p(2,1)= line_new(loop_round+j,x_inter(i));
-                    p(2,2)= line_new(loop_round+j,1+ x_inter(i));
+                    p(1,1) = line_new(loop_round,x_inter(i));
+                    p(1,2) = line_new(loop_round,1+x_inter(i));
+                    p(2,1) = line_new(loop_round+j,x_inter(i));
+                    p(2,2) = line_new(loop_round+j,1+ x_inter(i));
                     inter_point(k,i) = (p(1,1)*(p(2,2)-p(2,1))-p(2,1)*(p(1,2)-p(1,1))) / ...
                                        (p(2,2)-p(2,1)-p(1,2)+p(1,1));
                 end
@@ -165,7 +164,7 @@ function isovalue = get_isovalues(R,dim,options)
     isovalue_s = zeros(1,n_mnr);
     for k = 1:n_mnr-1
         for in = 1:n_mnr
-            in_mat(in,k)= intervalue(involved(in,k));
+            in_mat(in,k) = intervalue(involved(in,k));
             if 1 - max(in_mat(in,:)) > 1e-3
                 isovalue_s(in) = max(in_mat(in,:));
             else 
@@ -176,9 +175,9 @@ function isovalue = get_isovalues(R,dim,options)
     end
 
     % Closing any gaps
-    if gap_close == 1
+    if (gap_close == 1)
 
-        if n_mnr ==3 && range(isovalue_s) > 1e-4
+        if (n_mnr == 3) && (max(isovalue_s) - min(isovalue_s) > 1e-4)
 
             no_2 = zeros(1,n_mnr);
             for in = 1:n_mnr
@@ -187,99 +186,94 @@ function isovalue = get_isovalues(R,dim,options)
             end
 
             for in = 1:n_mnr-1
-                if isovalue_s(in)== no_2(in+1)
+                if isovalue_s(in) == no_2(in+1)
                     start_mat = find(diff(sign(isovalue_s(in+1)-line_new(in+1,:))));
                     intersect_val = (find(diff(sign(line_new(in,:)-line_new(in+1,:))), 1));
                     start_ind = start_mat(abs(start_mat - intersect_val)==min(abs(start_mat - intersect_val)));
-                    py1= line_new(in+1,start_ind);
-                    py2= line_new(in+1,1+start_ind);
-                    px1= start_ind;
-                    px2= start_ind+1;
+                    py1 = line_new(in+1,start_ind);
+                    py2 = line_new(in+1,1+start_ind);
+                    px1 = start_ind;
+                    px2 = start_ind+1;
                     y_int = isovalue_s(in+1);
                     x_int = px1 + ((y_int-py1)*(px2-px1))/(py2-py1);
 
-
-                    py1= line_new(in,start_ind);
-                    py2= line_new(in,1+start_ind);
-                    px1= start_ind;
-                    px2= start_ind+1;
+                    py1 = line_new(in,start_ind);
+                    py2 = line_new(in,1+start_ind);
+                    px1 = start_ind;
+                    px2 = start_ind+1;
                     y_new = py1 + ((x_int-px1)*(py2-py1))/(px2-px1);
                     isovalue_s(in) = y_new;
                 end
             end
 
             for in = 2:n_mnr
-                if isovalue_s(in)== no_2(in-1)
+                if (isovalue_s(in) == no_2(in-1))
                     start_mat = find(diff(sign(isovalue_s(in-1)-line_new(in-1,:))));
                     intersect_val = (find(diff(sign(line_new(in,:)-line_new(in-1,:))), 1));
                     start_ind = start_mat(abs(start_mat - intersect_val)==min(abs(start_mat - intersect_val)));
 
-                    py1= line_new(in-1,start_ind);
-                    py2= line_new(in-1,1+start_ind);
-                    px1= start_ind;
+                    py1 = line_new(in-1,start_ind);
+                    py2 = line_new(in-1,1+start_ind);
+                    px1 = start_ind;
 
-                    px2= start_ind+1;
+                    px2 = start_ind+1;
                     y_int = isovalue_s(in-1);
                     x_int = px1 + ((y_int-py1)*(px2-px1))/(py2-py1);
 
-
-                    py1= line_new(in,start_ind);
-                    py2= line_new(in,1+start_ind);
-                    px1= start_ind;
-                    px2= start_ind+1;
+                    py1 = line_new(in,start_ind);
+                    py2 = line_new(in,1+start_ind);
+                    px1 = start_ind;
+                    px2 = start_ind+1;
                     y_new = py1 + ((x_int-px1)*(py2-py1))/(px2-px1);
                     isovalue_s(in) = y_new;
                 end
             end
 
-            if isovalue_s(n_mnr) == no_2(1)
+            if (isovalue_s(n_mnr) == no_2(1))
                 start_mat = find(diff(sign(isovalue_s(1)-line_new(1,:))));
                 intersect_val = (find(diff(sign(line_new(n_mnr,:)-line_new(1,:))), 1));
-                start_ind = start_mat(abs(start_mat - intersect_val)==min(abs(start_mat - intersect_val)));
+                start_ind = start_mat(abs(start_mat - intersect_val) == min(abs(start_mat - intersect_val)));
 
-                py1= line_new(1,start_ind);
-                py2= line_new(1,1+start_ind);
-                px1= start_ind;
+                py1 = line_new(1,start_ind);
+                py2 = line_new(1,1+start_ind);
+                px1 = start_ind;
 
-                px2= start_ind+1;
+                px2 = start_ind+1;
                 y_int = isovalue_s(1);
                 x_int = px1 + ((y_int-py1)*(px2-px1))/(py2-py1);
 
-
-                py1= line_new(n_mnr,start_ind);
-                py2= line_new(n_mnr,1+start_ind);
-                px1= start_ind;
-                px2= start_ind+1;
+                py1 = line_new(n_mnr,start_ind);
+                py2 = line_new(n_mnr,1+start_ind);
+                px1 = start_ind;
+                px2 = start_ind+1;
                 y_new = py1 + ((x_int-px1)*(py2-py1))/(px2-px1);
                 isovalue_s(n_mnr) = y_new;
             end
 
-            if isovalue_s(1) == no_2(n_mnr)
+            if (isovalue_s(1) == no_2(n_mnr))
                 start_mat = find(diff(sign(isovalue_s(n_mnr)-line_new(n_mnr,:))));
                 intersect_val = (find(diff(sign(line_new(1,:)-line_new(n_mnr,:))), 1));
                 start_ind = start_mat(abs(start_mat - intersect_val)==min(abs(start_mat - intersect_val)));
 
-                py1= line_new(n_mnr,start_ind);
-                py2= line_new(n_mnr,1+start_ind);
-                px1= start_ind;
-                px2= start_ind+1;
+                py1 = line_new(n_mnr,start_ind);
+                py2 = line_new(n_mnr,1+start_ind);
+                px1 = start_ind;
+                px2 = start_ind+1;
                 y_int = isovalue_s(n_mnr);
                 x_int = px1 + ((y_int-py1)*(px2-px1))/(py2-py1);
 
-
-                py1= line_new(1,start_ind);
-                py2= line_new(1,1+start_ind);
-                px1= start_ind;
-                px2= start_ind+1;
+                py1 = line_new(1,start_ind);
+                py2 = line_new(1,1+start_ind);
+                px1 = start_ind;
+                px2 = start_ind+1;
                 y_new = py1 + ((x_int-px1)*(py2-py1))/(px2-px1);
                 isovalue_s(1) = y_new;
             end
         end
-
     end
 
     % Converting the isovalue
-    for in=1:n_mnr
+    for in = 1:n_mnr
         isovalue(in) = (isovalue_s(in)*l_length(in))/weight(in) +polmin(in);
     end
     
@@ -293,7 +287,7 @@ function isovalue = get_isovalues(R,dim,options)
         linestyles(2) = {':'};
         linestyles(3) = {'--'};
         for in = 1:n_mnr
-            plot(x_plot,line_new(in,:),'color',colors(in,:))
+            plot(x_plot,line_new(in,:),'color',colors(in,:));
         end
 
         clear yy
@@ -305,7 +299,7 @@ function isovalue = get_isovalues(R,dim,options)
         for in = 1:n_mnr
             plot(x_plot,yy(in,:),'color',colors(in,:),...
                  'LineWidth',1+n_mnr-in,'LineStyle',...
-                 cell2mat(linestyles(mod(in,3)+1)))
+                 cell2mat(linestyles(mod(in,3)+1)));
         end
 
         xlabel('Line Index')
@@ -314,17 +308,15 @@ function isovalue = get_isovalues(R,dim,options)
         legend_labels1 = strings(1,n_mnr);
         legend_labels2 = strings(1,n_mnr);
         mono_label = char(1,n_mnr);
-        for in=1:n_mnr
+        for in = 1:n_mnr
             mono_label(in) = char(in+'A'-1);
             legend_labels1(in) = strcat(mono_label(in),' block Rescaled Density');
             legend_labels2(in) = strcat(mono_label(in),' block Rescaled Isovalue');
         end
-
 
         legend([legend_labels1 legend_labels2])
         legend('Location','best')
         
         hold off;
     end
-
 end
